2f3d09e6045c292917948a6f9e89dc358fe65c92
MikeDev505 <54003204+MikeDev505@users.noreply.github.com>
2019-08-12 00:07:30 +0200

    Update reversingBuffer.cpp

--- use cases/speedtest/reversingBuffer.cpp
+++ use cases/speedtest/reversingBuffer.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+//simple aprouch
+// long long return and j is to avoid aggressive optimization
 long long reverseBuffor(char* buffor, int len, long long j)
 {
   int ret = 0;
@@ -18,6 +20,8 @@ long long reverseBuffor(char* buffor, int len, long long j)
   return ret;
 }
 
+// Fast verison used pointers
+// long long return and j is to avoid aggressive optimization
 long long reverseBufforFast(char* buffor, int len, long long j)
 {
   char* start = buffor;
@@ -36,6 +40,9 @@ long long reverseBufforFast(char* buffor, int len, long long j)
   return ret;
 }
 
+
+// Fastes verison used pointers and develop of inner loop
+// long long return and j is to avoid aggressive optimization
 long long reverseBufforFastFast(char* buffor, int len, long long j)
 {
   char* start = buffor;
@@ -115,24 +122,30 @@ long long reverseBufforFastFast(char* buffor, int len, long long j)
   return ret;
 }
 
+
 int main() {
     char bufor[10000];
     long long sumaRet=0;
-    auto t0 = std::chrono::high_resolution_clock::now();
     
+    //init buffer to avoid aggressive optimization
     for(int i=0; i< sizeof(bufor); i++)
     {
         bufor[i] = i%200;
     }
     
+    auto t0 = std::chrono::high_resolution_clock::now();
+    //testing execution loop
     for(long long i=0; i<10000;i++)
     {
         sumaRet += reverseBufforFastFast(bufor, sizeof(bufor),i);
-    }
-    
+    }    
     auto t1 = std::chrono::high_resolution_clock::now();    
     
+  
+  
+  
     int sum = 0;
+    //sum buffer to avoid aggressive optimization
     for(int i=0; i< sizeof(bufor); i++)
     {
         sum += bufor[i];
